2006185a67a27f6dd994da9c482fba112ec84a16
d3lliott <d3elliott2@gmail.com>
2021-01-25 19:39:30 -0500

    updated

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -46,12 +46,12 @@ namespace Method_Exercise
             //exercise 1
 
             var numberOfCars = Add(3, 9, 4); //uses Method Add from parameters
-            var numberOfTornadoes = Multi(7, 4, 5, 2); //uses Method Multi from parameters
+            var numberOfSalesman = Multi(7, 4, 5, 2); //uses Method Multi from parameters
             var numberOfTeams = Plus(4, 3);
             var numberOfPlayers = Times(6, 6);
 
             Console.WriteLine(numberOfCars);
-            Console.WriteLine(numberOfTornadoes);
+            Console.WriteLine(numberOfSalesman);
             Console.WriteLine(numberOfTeams);
             Console.WriteLine(numberOfPlayers);
             // ask about a players Name and some key stats
